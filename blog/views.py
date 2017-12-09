00c4364c58c62aeb700fc7d0b4044d5e424969b3
Emiliya <magan.97@mail.ru>
2017-12-09 10:20:03 +0500

    ass

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/5655.html', {'posts': posts})
     Post.objects.get(pk=pk)
 
 def post_detail(request, pk):
